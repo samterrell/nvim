987b6405018f9be50195c78553a99758de6040bb
Sam Terrell <samterrell@gmail.com>
2023-07-08 10:34:16 -0400

    Format on save

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -1,7 +1,24 @@
-local lsp = require('lsp-zero').preset({})
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+local lsp_format_on_save = function(bufnr)
+  vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    group = augroup,
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format()
+      filter = function(client)
+        return client.name == "null-ls"
+      end
+    end,
+  })
+end
+
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+  lsp.default_keymaps({ buffer = bufnr })
+  lsp_format_on_save(bufnr)
 end)
 
 lsp.setup()
@@ -12,7 +29,7 @@ local cmp_action = require('lsp-zero').cmp_action()
 cmp.setup({
   mapping = {
     -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
 
     -- Ctrl+Space to trigger completion menu
     ['<C-Space>'] = cmp.mapping.complete(),
